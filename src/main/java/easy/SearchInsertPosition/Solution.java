7982a22a54e98de2c79de7118aaecf9f616f051a
deniskovpaka <deniskovpaka@gmail.com>
2020-10-07 20:45:20 +0300

    added 35 solution

---
+++ src/main/java/easy/SearchInsertPosition/Solution.java
@@ -0,0 +1,26 @@
+package easy.SearchInsertPosition;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println("Res = " + new Solution().searchInsert(nums, target));
+    }
+}
