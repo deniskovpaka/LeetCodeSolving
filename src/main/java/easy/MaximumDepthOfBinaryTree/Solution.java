72f2ddf5890fe89b11c41e28c9185040d6d88640
deniskovpaka <deniskovpaka@gmail.com>
2020-10-09 09:31:20 +0300

    added 104 solution

---
+++ src/main/java/easy/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,15 @@
+package easy.MaximumDepthOfBinaryTree;
+
+import easy.FindModeInBinarySearchTree.TreeNode;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return Math.max(l, r) + 1;
+    }
+}
---
+++ src/main/java/easy/MergeSortedArray/Solution.java
@@ -0,0 +1,13 @@
+package easy.MergeSortedArray;
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6}; // [1,2,2,3,5,6]
+        new Solution().merge(nums1, 3, nums2, 3);
+        System.out.println("Res is");
+    }
+}
