565a03796c7384b0e491637c82605668027db126
deniskovpaka <deniskovpaka@gmail.com>
2022-05-10 23:16:35 +0300

    Added 392 Is Subsequence

---
+++ src/main/java/easy/IsSubsequence/Main.java
@@ -0,0 +1,10 @@
+package easy.IsSubsequence;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "axc";
+        String t = "ahbgdc";
+        Solution solution = new Solution();
+        System.out.println(solution.isSubsequence(s, t));
+    }
+}
